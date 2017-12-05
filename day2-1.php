dbfd07fce61a02c1f5ba8200a7e19e91236a3f83
kkj <kjell@nixlab.no>
2017-12-05 17:58:58 +0100

    Challenges

---
+++ day2-1.php
@@ -0,0 +1,14 @@
+<?php
+$handle = fopen("day2-1.csv", "r");
+$sum = 0;
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+        $split = str_getcsv($line, "\t");
+        natsort($split);
+        $sum += end($split) - reset($split);
+    }
+
+    fclose($handle);
+}
+
+print($sum);
---
+++ day2-2.php
@@ -0,0 +1,28 @@
+<?php
+$handle = fopen("day2-1.csv", "r");
+$sum = 0;
+$evenArr = array();
+if ($handle) {
+    while (($line = fgets($handle)) !== false) {
+        $split = str_getcsv($line, "\t");
+        foreach ( $split as $num )
+        {
+            foreach ( $split as $num2 ) {
+
+                if (($num % $num2 == 0) && ($num != $num2)) {
+                    array_push($evenArr, (int) $num);
+                    array_push($evenArr, (int) $num2);
+                }
+            }
+        }
+
+        natsort($evenArr);
+        var_dump($evenArr);
+        $sum += (end($evenArr) / reset($evenArr));
+        unset($evenArr);
+        $evenArr = array();
+    }
+
+    fclose($handle);
+}
+print($sum);
